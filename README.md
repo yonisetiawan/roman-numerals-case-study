77b4576de45c21d27d8d28b07a94b779fe91f1f6
Izhharuddin Syanmil <email": null>
2016-09-27 11:26:08 +0700

    tugas case study roman

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # roman-numerals-case-study
+solution 1:
+penggunaan array new untuk membuat array tidak efektif.
+penggunaan loop in loop pada for --> while sebaiknya dihindari, karena dapat membuat bingung pengguna maupun penulis lain.
+penggunaan if untuk menmbatasi kemampuan kode, sehingga tidak keluar dari kemampuan dan menyebabkan crash.
+solution 2:
+ada beberapa variabel yang belum dideklarasikan seperti key dan i.
+penggunaan perubahaan + pada variabel merupakan tambahan sederhana yang berguna tapi memang roman tidak punya krg dari 0.
+alternatif if dengan menggunakan atau menarik untuk digunakan.
+solution 3:
+penambahan  komen yang terlalu banyak malah membingungkan walaupun cukup membantu.
+mirip dengan solusi 1.
+ada yang sebenarnya tidak perlu di deklarasi num => numcopy
+solution 4:
+menurut saya pada solusi ini singkat dan jelas serta dapat lebih cepat.
+
+
+release 3:
+yang paling penting adalah membaca dengan seksama kode yang ada terlebih dahulu, pastikan lengkap, baru kemudian dibayangkan bagaimana proses berjalan dengan menggunakan contoh.
--- roman_numerals.js
+++ roman_numerals.js
@@ -3,8 +3,21 @@
 let assert = require('assert')
 
 // release 0
-function to_roman_old(arabic_num) {
+function to_roman_old(n) {
   // write your code here to convert arabic numerals into roman numerals
+  var arr = [];
+  var arrRoman = ["M", "DCCCC", "D", "CD", "C", "LXXXX", "L", "XL", "X", "V", "I"];
+  var arrArabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 5, 1];
+
+  for (var i = 0; i < arrArabic.length; i++) {
+    if (n >= arrArabic[i]) {
+      arr.push(arrRoman[i])
+      n = n - arrArabic[i]
+      i = i - 1
+    }
+  }
+  var hasil = arr.join("");
+  return (hasil)
 }
 
 // "assert" that the result of converting 1 using to_roman_old(1) should equal "I"
@@ -23,24 +36,55 @@ assert.equal(to_roman_old(5), "V")
 assert.equal(to_roman_old(6), "VI")
 
 // what other tests could you add to ensure your to_roman_old method is working?
-//
-// keep in mind that while some of the tests above seem useful, testing for
-// 1, 2, 3 and 4 doesn't really help you exercise more of your code. all of them
-// are basically testing the same thing, right?  it's only when you test the
-// arabic number 5 or 6 do you get new information about whether or not your
-// solution works
-//
-// building your sense of judgement about what and when to test doesn't come easy
-// take time writing your tests, reading other people's tests and building an
-// informed opinion from there
+assert.equal(to_roman_old(10), "X")
+assert.equal(to_roman_old(44), "XLIIII")
+assert.equal(to_roman_old(999), "DCCCCLXXXXVIIII")
+  // keep in mind that while some of the tests above seem useful, testing for
+  // 1, 2, 3 and 4 doesn't really help you exercise more of your code. all of them
+  // are basically testing the same thing, right?  it's only when you test the
+  // arabic number 5 or 6 do you get new information about whether or not your
+  // solution works
+  //
+  // building your sense of judgement about what and when to test doesn't come easy
+  // take time writing your tests, reading other people's tests and building an
+  // informed opinion from there
 
 //##############################################################################
 
-// release 1
-function to_roman_new(arabic_num) {
+// release 1 saya pilih solution 4 sebenarnya cmn males nulis ulangnya
+function to_roman_new(num) {
   // write your code here to convert arabic numerals into roman numerals
+  if (typeof num !== 'number') {
+    return false;
+  }
+  var digits = String(+num).split("");
+
+  var key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+    "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+    "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
+  ];
+
+  var roman_num = "";
+
+  var i = 3;
+  while (i--)
+
+    roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+
+
+  return Array(+digits.join("") + 1).join("M") + roman_num;
 }
 
 // you might be able to reuse some of your tests from above but take care not to
 // set the wrong expectations for yourself.  you wouldn't want to solve the wrong
 // problem, would you?
+assert.equal(to_roman_new(5), "V")
+assert.equal(to_roman_new(6), "VI")
+assert.equal(to_roman_new(10), "X")
+assert.equal(to_roman_new(44), "XLIV")
+assert.equal(to_roman_new(999), "CMXCIX")
+assert.equal(to_roman_new(2000), "MM")
+
+//Release 2
+// karena makin sedikit kode yang digunakan semakin mudah memaintencenya, jelas dan singkat.
+//masalah yang dipecah kecil2 memperjelas code
