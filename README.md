b1a880ffbceff010b3c1dbb40b82f2f1bfa6b077
Ahyana Rizky Pratama & YONI SETIAWAN <pair+ahyanarizky.yonisetiawan@devbootcamp.com>
2016-09-27 11:34:58 +0700

    Tugas Selesai

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # roman-numerals-case-study
+
+#solution 1 :
+=> deklarasi array tidak perlu menyertakan "new Array", bisa disederhanakan dengan langsung var array = isi array.
+=> solution 1 sudah betul dan berjalan sesuai harapan untuk range nilai 1-3999, diluar nilai tersebut function hanya akan return parameter.
+=> pertama, function akan cek nilai parameter
+jika nilai parameter diluar 1-3999, function akan langsung return parameter.
+jika nilai parameter antara 1-3999, function akan menyediakan variable baru yang berfungsi sebagai hasil yaitu romanNumeral = ""
+selanjutnya function akan track isi dari array roman, sambil proses track berjalan, kita set kondisi ketika value >= decimal[i], maka nilai value akan dikurangi decimal ke i dan variable romanNumeral akan diisi oleh roman ke i.
+untuk kondisi diluar itu, proses akan berjlan tanpa melakukan apapun.
+Terakhir tinggal return nilai dari roman numeral.
+ => kita bisa menambahkan fungsi sampai 5000 dengan catatan kita menambah isi dari aaray roman sesuai dengan symbol yang diperlukan
+ => function ini akan menghasilkan to roman modern, bukan to roman old
+ => cara untuk mengganti menjadi roman old hanya tinggal replace IV menjadi IIII dst.
+
+#solution 2 :
+=> function ini akan cek parameter, jika parameter bukan bertipe number, akan direturn dengan false.
+=> deklarasi variabel kurang semantic, meskipun bisa dijalankan namun lebih baik variable ditulis secara rapih dan semantic sehingga memudahkan orang lain untuk membaca code kita.
+=> parameter yang dimasukkan akan dirubah menjadi string dan kemudian di split
+=> parameter key di set untuk roman moderen, namun jika ingin merubah menjadi roman old cukup mengganti isi key dari IV jadi IIII dan IX jadi VIIII
+=> function ini menggunakan array key yang merupakan sebuah array panjang. Dari array key ini kemudian akan dibuat sebuah fungsi yang mengeluarkan index tertentu dari array key. Setelah itu hasil akan dikumpulkan lagi pada array baru dan di join sehingga menghasilkan hasil yang diharapkan.
+
+#solution 3 :
+=> function 3 menggunakan comment yang memudahkan orang membaca code, yang mana hal ini sangat bagus untuk dilakukan dan dibiasakan.
+=> function ini secara default tidak bisa dijalankan jika parameter diatas angka 999.
+=> function akan berjalan dengan looping decimalValue secara terus menerus selama kondisi numcopy > 0. saat pengecekan decimalValue berlangsung, kita set kondisi untuk mengeluarkan value dari romanNumeral sesuai dengan index dari kondisi yang terpenuhi. Selanjutnya jika kondisi terpenuhi, isi romanized dengan nilai dan kurangi angka numCopy sesuai index dari decimalValue.
+
+#solution 4 :
+=> function 4 merupakan function yang penulisannya paling pendek dan bisa mendapatkan hasil sesuai yang diharapkan. untuk memperpanjang range cukup hanya dengan menambahkan array decimal dan array roman.
+=> function ini bekerja dengan mengecek isi dari array decimal. ketika kondisi num%decimal[i] < num, result akan diisi oleh roman index ke i dan nilai num akan dikurangi oleh decimal indeks ke i.
+=> meskipun tidak ditulis comment, namun solution 4 ini menjadi solution yang paling mudah untuk dipahami karena menggunakan logika sederhana. 
--- roman_numerals.js
+++ roman_numerals.js
@@ -3,12 +3,86 @@
 let assert = require('assert')
 
 // release 0
-function to_roman_old(arabic_num) {
+// function to_roman_old(arabic_num) {
   // write your code here to convert arabic numerals into roman numerals
+  // var roman = new Array();
+  // roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+  // var decimal = new Array();
+  // decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  //
+  // function to_roman(value){
+  //   if(value <= 0 || value >= 4000) return value;
+  //   var romanNumeral = "";
+  //   for (var i=0; i<roman.length;i++){
+  //     while(value >= decimal[i]){
+  //       value -= decimal[i];
+  //       romanNumeral += roman[i];
+  //     }
+  //   }
+  //   return romanNumeral;
+  // }
+
+
+
+//   function to_roman(num) {
+//     if (typeof num!== 'number')
+//     return false;
+//
+//     var digits = String(+num).split(""), key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], roman_num = "" ,
+// i = 3;
+// while (i--)
+//   roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+//   return Array(+digits.join("") + 1).join("M") + roman_num;
+//   }
+
+
+// var to_roman = function(num){
+// var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000,4000];
+// var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+//
+// var numCopy = num;
+// var romanized = '';
+//
+// while(numCopy>0){
+//   for(var index =0; index<decimalValue.length; index++){
+//     if(+decimalValue[index]<= numCopy && +decimalValue[+index + 1] > numCopy){
+//       console.log("index : "+decimalValue[index]);
+//       console.log("index +1 : "+decimalValue[+index +1]);
+//       romanized += romanNumeral[index];
+//       numCopy -= decimalValue[index];
+//     }
+//   }
+// }
+//   return romanized;
+// }
+
+function to_roman(num){
+  var result = "";
+  var decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ["M","MC","D","CD","C","XC","L","XL","X", "IX", "V","IV","I"];
+  for (var i=0; i<=decimal.length;i++){
+    while(num%decimal[i]<num){
+      result += roman[i];
+      num -= decimal[i];
+    }
+  }
+  return result
 }
 
+
+  console.log("4     |  IV |" + to_roman(4));
+  console.log("9     |  IX |" + to_roman(9));
+  console.log("13    |  XIII |" + to_roman(13));
+  console.log("1453  |  MCDLIII |" + to_roman(1453));
+  console.log("1646  |  MDCXLVI |" + to_roman(1646));
+
+
+
+
+// }
+
 // "assert" that the result of converting 1 using to_roman_old(1) should equal "I"
-assert.equal(to_roman_old(1), "I")
+// console.log(assert.equal(to_roman(1), "I"));
 
 // alternatively, we can write the boolean expression ourselves like this:
 // assert(to_roman_old(1) == "I")
@@ -19,8 +93,8 @@ assert.equal(to_roman_old(1), "I")
 // assert.equal(to_roman_old(2), "II")    # commented out on purpose, see below
 // assert.equal(to_roman_old(3), "III")   # commented out on purpose, see below
 // assert.equal(to_roman_old(4), "IIII")  # commented out on purpose, see below
-assert.equal(to_roman_old(5), "V")
-assert.equal(to_roman_old(6), "VI")
+// assert.equal(to_roman(5), "V")
+// assert.equal(to_roman(6), "VI")
 
 // what other tests could you add to ensure your to_roman_old method is working?
 //
